5769f32a5bc8244c2c88acf4e49f478e75f3f524
Glenn Jahnke <gleenn@gmail.com>
2018-06-08 14:00:33 -0700

    wip s3

--- project.clj
+++ project.clj
@@ -8,6 +8,8 @@
                  [org.onyxplatform/onyx-sql "0.13.0.1"]
                  [org.postgresql/postgresql "42.2.2"]
 
+                 [org.onyxplatform/onyx-amazon-s3 "0.13.0.0"]
+
                  [com.stuartsierra/component "0.3.2"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
                    :plugins      [[lein-update-dependency "0.1.2"]
